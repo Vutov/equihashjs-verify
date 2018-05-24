022a960810c04a615d1a8b7050cbefe285586151
vutov <vutovspas@gmail.com>
2018-05-24 21:51:11 +0300

    Preparation for BTG Equihash params switch

--- src/networks.js
+++ src/networks.js
@@ -1,6 +1,18 @@
 module.exports = {
     bitcoingold: {
+        n: 144,
+        k: 5,
+    },
+    bitcoingoldPreEquihashFork: {
         n: 200,
         k: 9,
+    },
+    bitcoingoldTestnet: {
+        n: 96,
+        k: 5
+    },
+    bitcoingoldRegtest: {
+        n: 96,
+        k: 5
     }
 }
